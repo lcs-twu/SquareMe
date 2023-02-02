ecab36da16a48a0ccc86cff891b965a1348960b1
Tom <twu@lcs.on.ca>
2023-02-02 14:32:43 -0500

    final output to show

--- SquareMe/VIews/CalculationView.swift
+++ SquareMe/VIews/CalculationView.swift
@@ -10,15 +10,28 @@ import SwiftUI
 struct CalculationView: View {
     
     //MARK: Stored properties
+    //step1
     @State var inputGiven = ""
     
     //MARK: Computed properties
+    //step2
     //convert the input given to an optional double
     var inputGivenAsOptionalDouble: Double?{
         guard let inputGivenAsDouble = Double(inputGiven)else{
             return nil
         }
         return inputGivenAsDouble
+    }
+    //step3
+    //get a non optional value
+    var formattedOutPutValue: String{
+        //see if we have a number to work with
+        guard let numberToSquare = inputGivenAsOptionalDouble else{
+            return "Please enter a numeric value(eg. 5.0)"
+        }
+        let squareNumber = numberToSquare * numberToSquare
+    //step4
+        return squareNumber.formatted(.number.precision(.fractionLength(1)))
     }
     
     var body: some View {
@@ -27,6 +40,9 @@ struct CalculationView: View {
                 TextField("5.5", text: $inputGiven)
                     .font(.largeTitle)
                     .padding()
+                
+                Text(formattedOutPutValue)
+                    .font(.largeTitle)
             }
             .navigationTitle("Square Me")
         }
