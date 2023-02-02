aa534cdca8ff3450eba3ee51312b7965729df6a1
Tom <twu@lcs.on.ca>
2023-02-02 14:07:12 -0500

    create the view

--- SquareMe/SquareMeApp.swift
+++ SquareMe/SquareMeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SquareMeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CalculationView()
         }
     }
 }
---
+++ SquareMe/VIews/CalculationView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculationView.swift
+//  SquareMe
+//
+//  Created by Tom Wu on 2023-02-02.
+//
+
+import SwiftUI
+
+struct CalculationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculationView()
+    }
+}
