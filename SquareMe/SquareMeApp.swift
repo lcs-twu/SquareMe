6f2a5c71659eab9d0ae605b535c335598b1197ef
Tom <twu@lcs.on.ca>
2023-02-02 14:04:15 -0500

    Initial Commit

---
+++ SquareMe/SquareMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareMeApp.swift
+//  SquareMe
+//
+//  Created by Tom Wu on 2023-02-02.
+//
+
+import SwiftUI
+
+@main
+struct SquareMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
